6a604067843f53a359328f7e60970a442319bf6e
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-28 11:40:58 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,26 @@
+class Easy
+{
+ public static void main(String[] args) 
+ {
+  // Single dimension arry
+  int ar[]={1,5,2,4,6};
+   System.out.println("Value at ar[0]="+ar[0]); 
+   System.out.println("Value at ar[1]="+ar[1]);
+   System.out.println("Value at ar[2]="+ar[2]);
+   System.out.println("Value at ar[3]="+ar[3]);
+   System.out.println("Value at ar[4]="+ar[4]); 
+
+// Multi dimension arry 
+int a[][]={3,4,5},{5,6,7},{7,8,9}};
+System.out.println("value at a[0][0]="+a[0][0]);
+System.out.println("value at a[0][1]="+a[0][1]);
+System.out.println("value at a[0][2]="+a[0][2]);
+System.out.println("value at a[1][0]="+a[1][0]);
+System.out.println("value at a[1][1]="+a[1][1]);
+System.out.println("value at a[1][2]="+a[1][2]);
+System.out.println("value at a[2][0]="+a[2][0]);
+System.out.println("value at a[2][1]="+a[2][1]);
+System.out.println("value at a[2][2]="+a[2][2]);
+ }
+ }
+}
