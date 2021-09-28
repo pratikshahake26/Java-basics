67b22b2a5121cd6a465f707a3d4bec2da1fd6aef
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-28 11:25:12 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,26 @@
+class Easy
+{
+ public static void main(String[] args) 
+ {
+  int class=2;
+  switch(class)
+  {
+   case 1:
+    System.out.println("1st");
+    break;
+   case 2:
+    System.out.println("2nd");
+    break; 
+   case 3:
+    System.out.println("3rd");
+    break;
+   case 4:
+    System.out.println("4th");
+    break;
+   case 5:
+    System.out.println("5th");
+    break;
+   default:
+    System.out.println("No case matched");
+  }
+ }
