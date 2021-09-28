63c8835143802dd2e9b53c5dc8258948291cb245
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-28 11:22:13 +0530

    Create Nested if.java

---
+++ Nested if.java
@@ -0,0 +1,15 @@
+class Easy
+{
+ public static void main(String[] args) 
+ {
+  int no=6;
+  if(no>2)//true condition
+  {
+    if(no<3)//false condition
+    {
+     System.out.println("Hello1");
+    }
+    System.out.println("Hello2");
+  }
+ }
+}
