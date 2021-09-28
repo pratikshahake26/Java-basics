77b045201ac2357c053006b1db7605de308ac631
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-28 11:29:58 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,33 @@
+class Easy
+{
+ public static void main(String[] args) 
+ { 
+ 
+ // For loop
+  for (int i = 1; i <5; i++) 
+  {
+   System.out.println(i);   
+
+ // while loop   
+int x=1;
+  while(x<10)
+  {
+   System.out.println(x);
+   x++;
+
+ //do while loop 
+int x=1;
+  do
+  {
+   System.out.println(x);
+   x++;
+  }
+  while(x<10); 
+
+  // foreach loop
+int ar[]={1,2,3,4,5};
+  for (int element:ar) 
+   System.out.print(element+" ");
+  }
+ }
+}
