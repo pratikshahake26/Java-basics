31b796b1cf84ac1b291cfa62ae02481d5178dcf9
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-28 11:18:53 +0530

    Create Comment.java

---
+++ Comment.java
@@ -0,0 +1,13 @@
+class Easy
+{
+ public static void main(String[] args) 
+ { 
+   // Single line comment.
+     /*
+     This is multiline comment
+     Write a program to add 
+     two number and store it 
+     in third number.
+     */
+ }
+}
