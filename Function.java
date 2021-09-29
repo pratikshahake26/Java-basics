7a5552fc36870dd4b8aa176b98d23fa22a405dc0
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-29 08:45:57 +0530

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,14 @@
+class EASY
+ {
+  public int add(int y,int z)
+   {
+    int x;
+    x=y+z;
+    return x;
+   }
+  public static void main(String[] args) 
+   EASY obj=new EASY();
+   int rs=obj.add(10,20);
+   System.out.println("Add="+rs);
+  }
+ }
